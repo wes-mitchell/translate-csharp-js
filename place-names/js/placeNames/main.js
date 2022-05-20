a836def48a8fb688faa7af0c83a466add844ef9c
wes-mitchell <wmdrums@gmail.com>
2022-05-20 15:52:26 -0500

    solves place names exercise

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,21 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville", "Hong Kong", "The back yard", "Earth",
+   "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
-  console.log(name);
+ console.log(name);
+}
+
+console.log('');
+
+const startsWithThe = names.filter(name => name.startsWith("The"))
+
+console.log("'The' Place Names:");
+for (const place of startsWithThe) {
+ console.log(place)
 }
